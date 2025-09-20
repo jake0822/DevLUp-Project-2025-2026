f8371e6079e9c66d3bedcb9c2efd674b4d648a31
Miles Burkart <milesb2288@gmail.com>
2025-09-20 14:39:39 -0400

    Improve dash

--- Assets/Levels/Crystalline Cavern/Scripts/LightDash.cs
+++ Assets/Levels/Crystalline Cavern/Scripts/LightDash.cs
@@ -1,34 +1,59 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 
-public class LightDash : MonoBehaviour
-{
-    public CharacterController cc;
-    public Transform facingTransform;
+public class LightDash : MonoBehaviour {
+    public CharacterController playerController;
+    public CinemachineCamera playerFpsCamera;
+    public Transform playerFacingTransform;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [Header("Dash Settings")]
+    public int dashDuration = 40;  // The number of frames the player should dash for
+    public float dashStrength = 0.3f;  // The strength of the dash
+    public AnimationCurve dashStrengthCurve;  // Determines how the strength changes over the course of the dash
 
+    public float fovChange = 5.0f;  // How many degrees the FOV changes during a dash
+    public AnimationCurve fovCurve;  // Determines how the fov changes over the course of the dash
+
+    // Dash data
+    private float baseFov = 60;
+
+    private bool hasDash = false;
+    private bool isDashing = false;
+    private int dashTimer = 0;
+    private Vector3 dashVector;
+
+    void Start() {
+        baseFov = playerFpsCamera.Lens.FieldOfView;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        if (isDashing) {
+            float t = dashTimer / (float) dashDuration;
+            float dashStrengthT = dashStrengthCurve.Evaluate(t);
+            playerController.Move(dashVector * dashStrengthT);
 
-    }
+            float fovT = fovCurve.Evaluate(t);
+            float fov = Mathf.Lerp(baseFov + fovChange, baseFov, fovT);
+            playerFpsCamera.Lens.FieldOfView = fov;
 
-    public void Dash(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            Vector3 facingVector = facingTransform.rotation * Vector3.forward;
-            cc.Move(facingVector * 10);
+            dashTimer++;
+            if (dashTimer >= dashDuration) {
+                isDashing = false;
+            }
         }
-        else if (context.canceled)
-        {
-            Debug.Log("undash");
+        else if (playerController.isGrounded) {
+            hasDash = true;
+        }
+    }
+
+    public void Dash(InputAction.CallbackContext context) {
+        if (hasDash && context.performed) {
+            hasDash = false;
+            isDashing = true;
+            dashTimer = 0;
+            dashVector = playerFacingTransform.rotation * Vector3.forward * dashStrength;
         }
     }
 }
