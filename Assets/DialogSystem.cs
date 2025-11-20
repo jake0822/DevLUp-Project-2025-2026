fa207fa7319f2d091db64663654fa394fba9a092
Jake <user@FIH-COM23a111.fsu.edu>
2025-11-20 18:24:38 -0500

    adds dialog optins

---
+++ Assets/DialogSystem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class DialogSystem : MonoBehaviour
+{
+    [SerializeField] private string[] DialogLines;
+    public TextMeshProUGUI activeText;
+    public float textspeed = 0.1f;
+
+    public bool test = false;
+
+    private void Update()
+    {
+        if (!Application.isPlaying) return;
+
+        if (test)
+        {
+            test = false;
+            StartCoroutine(typeDialog(DialogLines[0]));
+
+        }
+    }
+
+    IEnumerator typeDialog(string text)
+    {
+        if (!Application.isPlaying) yield break;
+
+        int i = 0;
+        activeText.text = string.Empty;
+        while(i < text.Length)
+        {
+            activeText.text += text[i];
+            i++;
+            yield return new WaitForSeconds(textspeed);
+        }
+    }
+}
