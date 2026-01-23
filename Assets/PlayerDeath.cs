fec1243ef5832b50215c4cf7521b5e40f1f1ce34
Erica Beckendorf <user@FIH-COM23a110.fsu.edu>
2026-01-22 19:22:05 -0500

    adds sound effect skeleton for volcano

--- Assets/PlayerDeath.cs
+++ Assets/PlayerDeath.cs
@@ -1,14 +1,26 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class lavaDeath : MonoBehaviour
 {
+    public AudioSource audio;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+           // StartCoroutine(playDeathSound());
+            audio.Play();
             print("restart");
         }
+    }
+
+    private IEnumerator playDeathSound()
+    {
+        print("started");
+        yield return new WaitForSeconds(0.1f);
+        audio.Play();
+        print("death audio");
     }
 }
