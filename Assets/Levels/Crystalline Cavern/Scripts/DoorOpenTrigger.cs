269a6c4335559c75490599ce990d5987810355f9
Rubini 07 <user@FIH-COM23a107.fsu.edu>
2026-04-09 18:29:27 -0400

    Add door open trigger

---
+++ Assets/Levels/Crystalline Cavern/Scripts/DoorOpenTrigger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DoorOpenTrigger : MonoBehaviour
+{
+
+    [SerializeField] private UnityEvent doorOpenEvent;
+
+    private bool canOpenDoor = false;
+
+
+    public void SetCanOpenDoor()
+    {
+        canOpenDoor = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (canOpenDoor && other.CompareTag("Player"))
+        {
+            doorOpenEvent.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}
