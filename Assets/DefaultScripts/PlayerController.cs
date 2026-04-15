5c36d0a3f2badc54464953ae7ad7394df63e6e74
jake0822 <jakeyounan16@gmail.com>
2026-04-15 17:59:22 -0400

    updates hit box, adds things

--- Assets/DefaultScripts/PlayerController.cs
+++ Assets/DefaultScripts/PlayerController.cs
@@ -37,6 +37,7 @@ public class PlayerController : MonoBehaviour
     }
     private void Start()
     {
+        
         _collider = GetComponent<CapsuleCollider>();
         _controller = GetComponent<CharacterController>();
         
