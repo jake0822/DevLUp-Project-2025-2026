83eb3c32f972fe2a5b1b94e3d6f7608ca1e98ef3
Rubini 07 <user@FIH-COM23a107.fsu.edu>
2026-04-16 20:02:42 -0400

    Add particle to keys; make keys appear in door

--- Assets/Levels/Crystalline Cavern/Scripts/PlayerKeys.cs
+++ Assets/Levels/Crystalline Cavern/Scripts/PlayerKeys.cs
@@ -6,11 +6,15 @@ public class PlayerKeys : MonoBehaviour
     [SerializeField] private int totalKeys;
     [SerializeField] private UnityEvent collectAllEvent;
 
+    [SerializeField] private GameObject[] doorKeyObjects;
+
     private int keys = 0;
 
     public void AddKey()
     {
         keys++;
+        UpdateDoorKeyObjects();
+
         if (keys == totalKeys)
         {
             collectAllEvent.Invoke();
@@ -20,10 +24,20 @@ public class PlayerKeys : MonoBehaviour
     public void ResetKeys()
     {
         keys = 0;
+        UpdateDoorKeyObjects();
     }
 
     public bool HasAllKeys()
     {
         return keys == totalKeys;
+    }
+
+    void UpdateDoorKeyObjects()
+    {
+        for (int i = 0; i < totalKeys; i++)
+        {
+            bool active = i < keys;
+            doorKeyObjects[i].SetActive(active);
+        }
     }
 }
