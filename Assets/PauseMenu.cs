dbe9d71b2007a138ed1dda6880bf96e1f4051672
jake0822 <jakeyounan16@gmail.com>
2026-04-24 00:32:27 -0400

    updates the volume slider to work

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -8,13 +8,20 @@ public class PauseMenu : MonoBehaviour
     public PauseManager PauseManager;
     public Slider VolumeSlider;
 
-    public void AdjustVolume()
+    public void AdjustVolume(float value)
     {
-
+        AudioListener.volume = value;
+        PlayerPrefs.SetFloat("Volume", value);
+        PlayerPrefs.Save();
     }
 
     public void Resume()
     {
         PauseManager.SetPause(false);
+    }
+    
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
